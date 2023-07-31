ea0224bc916be2149071a6a3c63cf4f6db4e36a3
Nayama Rajlich <design@nayamarajlich.com>
2023-07-30 19:44:52 -0700

    first git

---
+++ script.js
@@ -0,0 +1,5 @@
+const accessKey = ""
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
