b98e799841fe1cc81b54211fd922389a2e6f79b7
Nayama Rajlich <design@nayamarajlich.com>
2023-08-01 21:20:56 -0700

    testing JS

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const accessKey = ""
+const accessKey = "bjzNqVF5awLsO28W0ePA50MuH2CosLRzSRLLycygK20"
 const formEl = document.querySelector("form")
 const inputEl = document.getElementById("search-input")
 const searchResults = document.querySelector(".search-results")
 const showMore = document.getElementById("show-more-button")
+
+let inputData = ""
+let page = 1;
+
+async function searchImages(){
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${input}&client_id=${accessKey}` 
+    const response = await fetch(url)
+    const data = await response.json()
+}
